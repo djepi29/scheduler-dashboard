7913564a9630e9bb05fef6570e4e236570e41e9e
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-06-19 16:46:47 -0400

    mount loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
+import Loading from "./Loading";
+
 class Dashboard extends Component {
 
   state = {
-    loading: true
+    loading: false
   }
 
 
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading/>
+    }
 
     return <main className={dashboardClasses} />;
   }
